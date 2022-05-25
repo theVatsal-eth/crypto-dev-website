ec694226696c4daa50e773836672ee32cc6eb562
theVatsal-eth <0505m2003@gmail.com>
2022-05-25 11:08:18 +0530

    Error Handled

--- pages/api/[tokeId].ts
+++ pages/api/[tokeId].ts
@@ -1,11 +1,22 @@
-export default function handler(req, res) {
-    const tokenId = req.query.tokenId;
-    const image_url =
-      "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
-    res.status(200).json({
-      name: "Crypto Dev #" + tokenId,
-      description: "Crypto Dev is a collection of developers in crypto",
-      image: image_url + tokenId + ".svg",
-    });
+export default function handler(
+  req: { query: { tokenId: any } },
+  res: {
+    status: (arg0: number) => {
+      (): any;
+      new (): any;
+      json: {
+        (arg0: { name: string; description: string; image: string }): void;
+        new (): any;
+      };
+    };
   }
-  
+) {
+  const tokenId = req.query.tokenId;
+  const image_url =
+    "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+  res.status(200).json({
+    name: "Crypto Dev #" + tokenId,
+    description: "Crypto Dev is a collection of developers in crypto",
+    image: image_url + tokenId + ".svg",
+  });
+}
